67daa2a1e044de52aa2499e1b782be841008fc42
Raul <dukecapitan@gmail.com>
2022-03-19 21:29:06 +0100

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TasksComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule
